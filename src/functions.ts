17573cacd8579f7a9577dbdacf13ecfe18ed684e
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-02-22 02:12:07 -0300

    Exercicio 4

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,8 @@
 export function first<Type>(arr: Type[]): Type {
-  return arr[0]
-};
+  return arr[0];
+}
+
+export function updateItem<Type>(arr: Type[], index: number, ob: Type): Type[] {
+  if (index >= 0 && index < arr.length) arr.splice(index, 1, ob);
+  return arr;
+}
