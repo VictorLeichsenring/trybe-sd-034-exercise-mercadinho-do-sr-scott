b46f9bc08ed8e168bba40f0f00695c33f3b3de71
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-02-22 02:19:19 -0300

    exercicio 6

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+import { CartItem } from './types/CartItem';
+import { Drink } from './types/Drink';
+import { Food } from './types/Food';
+
 export function first<Type>(arr: Type[]): Type {
   return arr[0];
 }
@@ -5,4 +9,8 @@ export function first<Type>(arr: Type[]): Type {
 export function updateItem<Type>(arr: Type[], index: number, ob: Type): Type[] {
   if (index >= 0 && index < arr.length) arr.splice(index, 1, ob);
   return arr;
+}
+
+export function buildCartItem({ name, price }: Food | Drink, quantity: number): CartItem {
+  return { name, price, quantity } as CartItem;
 }
